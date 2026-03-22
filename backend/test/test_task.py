20a030801144f9f95cd4c4b473181b2a4edb541b
Kuroome <dethe3255@gmail.com>
2026-03-22 19:18:14 +0800

    v2.3.11a

--- backend/test/test_task.py
+++ backend/test/test_task.py
@@ -1,12 +1,13 @@
-import asyncio
 
 from app.configs.config import get_settings
-from app.utils.mailservice import send_bootstrap_emails
+
+# from app.utils.mailservice import send_bootstrap_emails
 
 get_settings.cache_clear()
 
 
 if __name__ == "__main__":
-    # 测试发送引导邮件
-    test_email = "kano3255@outlook.com"
-    asyncio.run(send_bootstrap_emails(test_email))
+    # 测试发送引导邮件 [已禁用]
+    # test_email = "kano3255@outlook.com"
+    # asyncio.run(send_bootstrap_emails(test_email))
+    pass
