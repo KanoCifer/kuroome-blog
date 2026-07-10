e2b604ed299352837b4ded17c803d86f2c52b2fd
Kuroome <dethe3255@gmail.com>
2026-07-11 00:40:13 +0800

    fix:cors err  - try restore cors

--- go-backend/internal/router/router.go
+++ go-backend/internal/router/router.go
@@ -7,6 +7,7 @@ package router
 import (
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
 
@@ -19,7 +20,7 @@ import (
 func Setup(r *gin.Engine, state *app.AppState, redis *redis.Client) {
 	r.Use(middleware.Duration())
 	r.Use(middleware.Trace())
-	// r.Use(cors.New(middleware.NewCORSConfig()))
+	r.Use(cors.New(middleware.NewCORSConfig()))
 	v3 := r.Group("/api/v3")
 
 	// 限流: 登录 / 注册各 5 次 / 分钟。
